ec23137fa0868239312f051d4afc601794f7d20d
Luana Silva <145373455+luamdrs@users.noreply.github.com>
2024-09-11 19:47:10 -0300

    🎉

---
+++ Exercicios_Python/Exercício_107.py/moeda.py
@@ -0,0 +1,13 @@
+def aumentar(preco, taxa):
+    valor = preco + (preco * taxa/100)
+    return valor
+
+
+def dobro(preco):
+    valor = preco * 2
+    return valor
+
+
+def metade(preco):
+    valor = preco / 2
+    return valor
