bc8fe8d83928981de38a791f15075e22a08d033b
Luana Silva <145373455+luamdrs@users.noreply.github.com>
2024-09-12 20:35:43 -0300

    (❤️ ω ❤️)

---
+++ Exercicios_Python/Exercicio_111.py/main.py
@@ -0,0 +1,4 @@
+from utilidades import moeda
+
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p, 10, 5)
---
+++ Exercicios_Python/Exercicio_111.py/utilidades/moeda/__init__.py
@@ -0,0 +1,39 @@
+# Função de Porcentagem(aumentar)
+def aumentar(preco=0, taxa=0, formato=False):
+    valor = preco + (preco * taxa / 100)
+    return valor if formato is False else moeda(valor)
+
+
+# Função de Porcentagem(diminuir)
+def diminuir(preco=0, taxa=0, formato=False):
+    valor = preco - (preco * taxa / 100)
+    return valor if formato is False else moeda(valor)
+
+
+# Função de dobro
+def dobro(preco=0, formato=False):
+    valor = preco * 2
+    return valor if formato is False else moeda(valor)
+
+
+# Função de metade
+def metade(preco=0, formato=False):
+    valor = preco / 2
+    return valor if formato is False else moeda(valor)
+
+
+# Função de formatação de moeda
+def moeda(preco=0, moed='R$'):
+    return f'{moed}{preco:.2f}'.replace('.', ',')
+
+
+# Função resumo (taxa aumento e redução)
+def resumo(preco=0, taxa_aum=10, taxa_red=5):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'Com {taxa_aum}% de aumento: \t{aumentar(preco, taxa_aum, True)}')
+    print(f'Com {taxa_red}% de redução: \t{diminuir(preco, taxa_red, True)}')
